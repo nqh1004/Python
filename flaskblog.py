6180b48e3571b33d5c9eb058237131149c11a334
nqh1004 <nqh1004.fw@gmail.com>
2024-01-11 09:11:08 -0500

    Update

--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,28 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 app = Flask(__name__)
 
+
+posts = [
+	{
+		'author': 'Corey Schafer',
+		'title': 'Blog Posts 1',
+		'content': 'First post content',
+		'date_posted': 'April 20, 2018'
+	},
+	{
+		'author': 'Corey Schafer',
+		'title': 'Blog Posts 2',
+		'content': 'Second post content',
+		'date_posted': 'April 21, 2018'
+	}
+]
 @app.route("/")
 @app.route("/home")
 def home():
-	return render_template('home.html')
+	return render_template('home.html',posts=posts)
 @app.route("/about")
 def about():
-	return render_template('about.html')
+	return render_template('about.html', title='About')
 
 if __name__ == '__main__':
 	app.run(debug=True)
